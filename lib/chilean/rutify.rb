09a810b60a2e6f4dcba46faedb227c7d0b419181
Mihail Pozarski <mihailpozarski@outlook.com>
2021-04-28 19:21:59 -0400

    feat(Rutify): allow integers as ruts

--- lib/chilean/rutify.rb
+++ lib/chilean/rutify.rb
@@ -10,15 +10,18 @@ module Chilean
   module Rutify
     # checks if the passed value is valid as a rut character
     def valid_rut_value?(rv)
-      ['0','1','2','3','4','5','6','7','8','9','k','K','.','-'].include?(rv)
+      ['0','1','2','3','4','5','6','7','8','9','k','K','.','-'].include?(rv.to_s)
     end
 
     # returns the rut verifier value
-    def get_verifier(rut)
+    def get_verifier(raw_rut)
+      rut = stringify_rut(raw_rut)
+      return unless rut.present? && valid_rut_values?(rut)
+
       sum = 0
       mul = 2
 
-      rut.to_s.reverse.split("").each do |i|
+      rut.reverse.split("").each do |i|
         sum += i.to_i * mul
         mul = mul == 7 ? 2 : mul + 1
       end
@@ -38,15 +41,16 @@ module Chilean
     # checks if the passed rut has a proper verifier value
     def valid_rut_verifier?(raw_rut)
       rut = normalize_rut(raw_rut)
-      return false if rut.empty? || rut.size < 2 || !rut.is_a?(String)
+      return false unless rut.present? && valid_rut_values?(rut)
 
       r = rut[0..(rut.size - 2)]
       get_verifier(r) == rut[-1]
     end
 
     # returns the rut value without any sepecial caracter and upcased
-    def normalize_rut(rut)
-      return if rut.nil? || !rut.is_a?(String) || !valid_rut_values?(rut)
+    def normalize_rut(raw_rut)
+      rut = stringify_rut(raw_rut)
+      return unless rut.present? && valid_rut_values?(rut)
 
       rut = rut.delete "."
       rut = rut.delete "-"
@@ -56,7 +60,7 @@ module Chilean
     # returns the rut value with the chilean format
     def format_rut(raw_rut)
       rut = normalize_rut(raw_rut)
-      return if rut.nil? || rut.empty? || rut.size < 2
+      return unless rut.present? && valid_rut_values?(rut)
 
       verifier = rut[-1]
       temp_rut = rut[0..-2]
@@ -74,19 +78,31 @@ module Chilean
     # checks if the passed rut is valid
     def valid_rut?(raw_rut)
       rut = normalize_rut(raw_rut)
-      return false if rut.nil? || rut.empty? || rut.size < 2
-      return false unless valid_rut_values?(rut)
+      return false unless rut.present? && valid_rut_values?(rut)
 
       valid_rut_verifier?(rut)
     end
 
+    # checks if all the rut values are valid
     def valid_rut_values?(raw_rut)
-      raw_rut.split("").each do |i|
+      rut = stringify_rut(raw_rut)
+      return false unless rut.present?
+
+      rut.split("").each do |i|
         return false unless valid_rut_value?(i)
       end
+
+      true
+    end
+
+    # checks rut minimum requirements and transform value to string if posible
+    def stringify_rut(raw_rut)
+      return if raw_rut.nil? || (!raw_rut.is_a?(String) && !raw_rut.is_a?(Integer))
+
+      raw_rut.to_s
     end
 
     module_function :normalize_rut, :format_rut, :valid_rut_value?, :get_verifier, :valid_rut?, :valid_rut_verifier?,
-                    :valid_rut_values?
+                    :valid_rut_values?, :stringify_rut
   end
 end
