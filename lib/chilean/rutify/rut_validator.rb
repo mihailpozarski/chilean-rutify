97153797332ae2ed9f968fdec3609c8bf650aac6
Mihail Pozarski <mihailpozarski@outlook.com>
2022-08-11 18:25:08 -0400

    fix(RutValidator): validators shouldn't validate presence

--- lib/chilean/rutify/rut_validator.rb
+++ lib/chilean/rutify/rut_validator.rb
@@ -3,10 +3,9 @@
 # Validates that the given string has the correct rut syntax
 class RutValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    if value.nil? || !value.is_a?(String)
-      record.errors.add attribute, (options[:message] || "is a empty rut")
-    elsif !Chilean::Rutify.valid_rut?(value)
-      record.errors.add attribute, (options[:message] || "is not a valid rut")
-    end
+    return if value.blank?
+    return if Chilean::Rutify.valid_rut?(value)
+
+    record.errors.add attribute, (options[:message] || "is not a valid rut")
   end
 end
